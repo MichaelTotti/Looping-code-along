5cf3eb93d83b4419549ec43a3d57b8ea17c19314
Michael <michael.onyango2@student.moringaschool.com>
2025-04-01 22:19:33 +0300

    1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(){
+    const names = ["Guadalupe", "Ollie", "Aki"];
+    const messages = [];
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+function countDown(){
+    let countDown = 10;
+    while (countDown >= 0){
+        console.log(countDown);
+        countDown--;
+    }
+    return countDown;
+}
